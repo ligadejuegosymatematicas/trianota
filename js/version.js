09a6d5f89a4afd45895345cd28ffd13d4cac3386
ligadejuegosymatematicas <ligadejuegosymatematicas@outlook.com>
2026-07-05 14:24:25 -0400

    Add files via upload

---
+++ js/version.js
@@ -0,0 +1,5 @@
+window.TRIANOTA_VERSION = '19.78';
+window.TRIANOTA_BUILD = 'player_stats_min';
+window.TRIANOTA_VERSION_LABEL = '19.78';
+window.TRIANOTA_CACHE_NAME = 'trianota-pwa-v19.78-player-stats-min';
+window.TRIANOTA_CACHE_BUST = '19.78_player_stats_min';
