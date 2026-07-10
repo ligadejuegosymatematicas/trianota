bc9a5ca76c77779bc255556f800c65f8f9ce7a2d
ligadejuegosymatematicas <ligadejuegosymatematicas@outlook.com>
2026-07-09 20:59:52 -0400

    Add files via upload

--- js/version.js
+++ js/version.js
@@ -1,5 +1,5 @@
-﻿window.TRIANOTA_VERSION = '19.85_config_auto_ui';
-window.TRIANOTA_BUILD = 'config_auto_ui';
-window.TRIANOTA_VERSION_LABEL = '19.85';
-window.TRIANOTA_CACHE_NAME = 'trianota-pwa-v19.85-config-auto-ui';
-window.TRIANOTA_CACHE_BUST = '19.85_config_auto_ui';
+window.TRIANOTA_VERSION = '19.87_modales_premium';
+window.TRIANOTA_BUILD = 'modales_premium';
+window.TRIANOTA_VERSION_LABEL = '19.87';
+window.TRIANOTA_CACHE_NAME = 'trianota-pwa-v19.87-modales-premium';
+window.TRIANOTA_CACHE_BUST = '19.87_modales_premium';
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = 'trianota-pwa-v19.85-config-auto-ui';
+const CACHE_NAME = 'trianota-pwa-v19.87-modales-premium';
 const LOCAL_ASSETS = [
   './',
   './index.html',
