6f9114bd759c236d7cf42ac63d1f89f4266def02
ligadejuegosymatematicas <ligadejuegosymatematicas@outlook.com>
2026-07-11 20:54:23 -0400

    Add files via upload

--- js/version.js
+++ js/version.js
@@ -1,5 +1,5 @@
-window.TRIANOTA_VERSION = '20.02_navigation_polish';
-window.TRIANOTA_BUILD = 'navigation_polish';
-window.TRIANOTA_VERSION_LABEL = '20.02';
-window.TRIANOTA_CACHE_NAME = 'trianota-pwa-v20.02-navigation-polish';
-window.TRIANOTA_CACHE_BUST = '20.02_navigation_polish';
+window.TRIANOTA_VERSION = '20.03_home_final';
+window.TRIANOTA_BUILD = 'home_final';
+window.TRIANOTA_VERSION_LABEL = '20.03';
+window.TRIANOTA_CACHE_NAME = 'trianota-pwa-v20.03-home-final';
+window.TRIANOTA_CACHE_BUST = '20.03_home_final';
